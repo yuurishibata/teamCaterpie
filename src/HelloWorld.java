3d74f61218298fd0c2e63485c50eaba53fcbfa30
morishi0913 <morishigebros@hotmail.com>
2013-10-25 18:35:33 +0900

    test test

---
+++ src/HelloWorld.java
@@ -0,0 +1,27 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("HelloWorld!:)");
+
+	Sample morisige = new Sample();
+	
+	morisige.setName("morisige");
+	System.out.println(morisige.getName());
+
+	Sample a = new Sample();
+	a.setName("Taro");
+	System.out.println(a.getName());
+	
+	Sample b = new Sample();
+	b.setName("Jiro");
+	System.out.println(b.getName());
+	
+	Sample c = new Sample();
+	c.setName("Saburo");
+	System.out.println(c.getName());
+	
+	}
+
+}
