4c67c942b3b7317aeea34da801dfc23d25e79deb
Takamichi Ohsumi <s12173to@sfc.keio.ac.jp>
2013-10-26 08:58:25 +0000

    algorithm class

--- src/AlgorithmStrategy.java
+++ src/AlgorithmStrategy.java
@@ -2,7 +2,12 @@ import java.util.ArrayList;
 
 public class AlgorithmStrategy {
 
+    ArrayList<Entry> inputEntries;
+    ArrayList<Entry> outputEntries;
+
     public AlgorithmStrategy(){
+	inputEntries = new ArrayList<Entry>();
+	outputEntries = new ArrayList<Entry>();
     }
     public ArrayList<Entry> process(){
     }
