2d17d4e4e3bc296584cb718f6bbfecffedefbe9c
Takamichi Ohsumi <s12173to@sfc.keio.ac.jp>
2013-10-27 07:36:55 +0000

    引数の改良

--- src/AlgorithmStrategy.java
+++ src/AlgorithmStrategy.java
@@ -2,8 +2,10 @@ import java.util.ArrayList;
 
 public class AlgorithmStrategy {
 
-     public AlgorithmStrategy(){
+    public AlgorithmStrategy(User user,ArrayList<Entry> entries){
+
     }
+
     public ArrayList<Entry> process(){
 
 	return new ArrayList<Entry>();
