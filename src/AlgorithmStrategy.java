a2b13f77de8da779a84ebea6664b0841358965a3
Takamichi Ohsumi <s12173to@sfc.keio.ac.jp>
2013-10-27 07:41:42 +0000

    AlgorithmStrategyクラスの再編

--- src/AlgorithmStrategy.java
+++ src/AlgorithmStrategy.java
@@ -2,7 +2,14 @@ import java.util.ArrayList;
 
 public class AlgorithmStrategy {
 
+    private ArrayList<Entry> inputEntries = new ArrayList<Entry>();
+    private ArrayList<Entry> outputEntries = new ArrayList<Entry>();
+    User user = new User();
+
     public AlgorithmStrategy(User user,ArrayList<Entry> entries){
+
+	this.inputEntries  = entries;
+	this.user = user;
 
     }
 
