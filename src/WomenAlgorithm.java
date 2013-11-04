9d0f1054ae64aaad560853f45e971495ba0e9a7d
Takamichi Ohsumi <ohsumi@users.noreply.github.com>
2013-11-04 12:39:42 +0900

    Update WomenAlgorithm.java

--- src/WomenAlgorithm.java
+++ src/WomenAlgorithm.java
@@ -2,7 +2,7 @@ public class WomenAlgorithm implements AlgorithmInterface {
 
     private  String title;
     private String content;
-    private String[] keywords = { "ワンピース", "ワンピ", "スカート", "ブラウス", "ショーパン",
+    private String[] keywords = { "ワンピ", "スカート", "ブラウス", "ショーパン",
 			  "ワンピース", "かわい", "可愛", "カワイ", "女性", "フェミニン", "私", "女の子" };
 
     public WomenAlgorithm(String title, String content) {
