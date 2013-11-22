5f05a2ad6eea4807a185b9414abc6dc434fb46af
Takamichi Ohsumi <s12173to@sfc.keio.ac.jp>
2013-11-22 11:27:46 +0000

    テストの新しいバージョン

--- src/HashCodeDBTest.java
+++ src/HashCodeDBTest.java
@@ -3,23 +3,28 @@ public class HashCodeDBTest {
     public static void main(String[] args) {
 
 	// テスト関数
-
-	// データベースインスタンス作成
 	HashCodeDB hcdb = new HashCodeDB();
-	// そのインスタンスのgetRDataメソッドに引数を渡しRの統計データを取得する。
-	RData rd = hcdb.getRData("女性", "フリル");
-	// そのRのデータにアクセスする。
+
+	RData rd = hcdb.getRData("レース", "胸元");
+
 	System.out.println(rd.getConfidence());
-	RData rd3 = hcdb.getRData("リボン", "ワンピ");
+	System.out.println(rd.getSupport());
+	System.out.println(rd.getLift());
 
-	System.out.println(rd3.getSupport());
-	System.out.println(rd3.getLift());
-	System.out.println(rd3.getConfidence());
+	System.out.println(hcdb.getData("男性"));
 
-	// 他のデータの取得（１つの形態素から、登録された実数１つを取り出す方法）
-	System.out.println(hcdb.getPoint("ワンピース"));
-	System.out.println(hcdb.getPoint("男性"));
-	System.out.println(hcdb.getPoint("ソフトウェア開発"));// 登録されていない形態素は「zero」を返却する。
+	System.out.println(hcdb.existRData("リボン", "ワンピ"));
+	System.out.println(hcdb.existRData("女性", "フリル"));
+	System.out.println(hcdb.existRData("レース", "軍"));
+
+	ArrayList<String> ma = new ArrayList<String>();
+	ma.add("女性");
+	ma.add("男性");
+	ma.add("フリル");
+
+	RData rd2 = hcdb.getRData(ma);
+
+	System.out.println(rd2.getConfidence());
 
     }
 }
