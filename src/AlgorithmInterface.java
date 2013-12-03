7f8b6a3ff9f8b25a1e02cf200dfde68a1397cf8e
Takamichi Ohsumi <ohsumi@users.noreply.github.com>
2013-12-03 11:26:09 +0900

    Delete AlgorithmInterface.java

--- src/AlgorithmInterface.java
+++
@@ -1,8 +0,0 @@
-public interface AlgorithmInterface {
-    //アルゴリズム処理をするメソッド
-    public void process();
-    //情報を出力するメソッド
-    public void print();
-    //アルゴリズムの結果を返却するメソッド
-    public int getResult();
-}
