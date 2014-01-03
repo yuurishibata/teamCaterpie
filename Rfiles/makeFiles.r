e970faf162a47e898ecbf4a1e4cbdede4c5bb513
Takamichi Ohsumi <s12173to@sfc.keio.ac.jp>
2014-01-03 02:21:14 +0000

    語彙頻度集計表の作成を自動化。

--- Rfiles/makeFiles.r
+++ Rfiles/makeFiles.r
@@ -43,6 +43,72 @@ removeURLs <- function(x){
 #URLを削除する
 allblogs <- removeURLs(allblogs)
 
+###ここに記号処理のプログラムを挿入する。
+
+#不要語削除の鍵を握るプログラム
+#正規表現を第二引数に指定しても機能する
+removeCharacter <- function(string,character){ 
+
+ while((index <- c(unlist(regexpr(character,string))))!=-1){
+
+  #文章前半
+  X <- substring(string,0,index-1)
+  #文章後半
+  Y <- substring(string,index+1,nchar(string))
+  #文章の再構成
+  string <- paste(X,Y,sep="")
+
+  }
+
+ return(string)
+
+}
+#複数の不要語を削除するプログラム
+#引数に受け取るブログは1つのみ
+
+removeCharacters <- function(blog){
+
+ regs <- c("[[:digit:]]","[[:punct:]]")
+ unnecessaryWords <- c("★","☆","。","●","○")
+
+ for(i in 1:length(regs)){
+
+ blog <- removeCharacter(blog,regs[i])
+
+ }
+
+ for(i in 1:length(unnecessaryWords)){
+
+ blog <- removeCharacter(blog,unnecessaryWords[i])
+
+ }
+
+ return(blog)
+
+}
+
+#複数のブログから不要な数値・記号を取り除く処理を行う。
+
+removeCharactersFromBlogs <- function(allblogs){
+
+
+ times <- length(allblogs)
+
+ for(i in 1:times){
+
+ allblogs[i] <- removeCharacters(allblogs[i])
+
+ }
+
+ return(allblogs)
+
+}
+
+#上のremoveCharactersFromBlogs関数の利用
+
+allblogs <- removeCharactersFromBlogs(allblogs)
+
+
 #ファイルの作成。ブログのデータの個数分
 makefiles <- function(allblogs,path){
 
@@ -68,5 +134,61 @@ makefiles <- function(allblogs,path){
 #これによってRMeCabﾗｲﾌﾞﾗﾘの諸関数を実行することが出来る。
 makefiles(allblogs,dirpath)
 
-#これによって、クラスター解析や潜在意味インデキシングなどの
-#関数を利用するための準備が出来た。
+#先ほど作成した対象ディレクトリのパスを取得する関数
+getPath <- function(dirname){
+
+ #パスの作成
+ subjectDir <- getwd()
+ subjectDir <- paste(subjectDir,"/",sep="")
+ subjectDir <- paste(subjectDir,dirname,sep="")
+ print(subjectDir)
+
+}
+
+#悲劇。語彙頻度集計表を作成するにあたり、ディレクトリの新規作成は
+#不必要だということが分かった。
+
+#整序された語彙頻度集計表を作成する関数
+getPath <- function(dirname){
+
+
+ subjectDir <- getwd()
+ subjectDir <- paste(subjectDir,"/",sep="")
+ subjectDir <- paste(subjectDir,dirname,sep="")
+
+ return(subjectDir)
+ 
+}
+
+#パスに指定されたファイルの語彙頻度集計表を整序した形で作成する関数
+getSortedWordFreq <- function(filename){
+
+ 
+ path <- getPath(filename) 
+ wordFreq <- RMeCabFreq(path)
+
+ #wordFreqオブジェクトから
+ #"名詞"・"形容詞"・"動詞"・"助動詞"を取り出して
+ #かつ、細分類が"非自立"でないもののみを抽出する
+
+ wordFreq <- wordFreq[(wordFreq$Info1=="名詞"|
+                       wordFreq$Info1=="形容詞"|
+                       wordFreq$Info1=="動詞"|
+                       wordFreq$Info1=="助動詞")
+                       &(wordFreq$Info2 != "非自立"),]
+
+ #テキストデータ全体で少なくとも3回の単語の出現
+ #がなければ、集計表のリストに載らない
+ wordFreq <- wordFreq[wordFreq$Freq > 3, ]
+ #ソート作業
+ wordFreq <- wordFreq[rev(order(wordFreq$Freq)),]
+ #結果返却
+ return(wordFreq)
+
+}
+
+#この中で正規表現でアルファベットのものとパンクチュエーション
+#を取り除いた表を作る関数
+
+#さらに、ひとが必要ないと捉える「あれ」「これ」「こと」などを削除
+#していく関数が必要になるのでそれを追加する。
